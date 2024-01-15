8daddc17079172237353910e06d98a7b1e8a5a44
Francisco Campos <zathangel@hotmail.com>
2024-01-14 21:52:45 -0300

    defining index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,22 @@
-precio = 400000
+const precio = 1000
 
-precioSpan = document.querySelector(".precio-inicial");
+const precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+const total = document.querySelector('.valor-total')
+const cantidad = document.querySelector('.cantidad')
+const sumar = document.querySelector('.boton1')
+const restar = document.querySelector('.boton2')
+
+sumar.addEventListener('click', function(){
+    cantidad.innerHTML = parseInt(cantidad.innerHTML) + 1;
+    total.innerHTML = precio * parseInt(cantidad.innerHTML)
+})
+
+restar.addEventListener('click', function(){
+    if (parseInt(cantidad.innerHTML) > 0) {
+        cantidad.innerHTML = parseInt(cantidad.innerHTML) - 1;
+        total.innerHTML = precio * parseInt(cantidad.innerHTML)
+    }
+  
+})
